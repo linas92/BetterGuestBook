9638ca693e6818ba73bbdf809ecf8cbce47bc72c
Linas Norkus <linasnorkus92@gmail.com>
2023-04-27 11:39:26 +0300

    yes

--- Program.cs
+++ Program.cs
@@ -1,10 +1,50 @@
-﻿namespace BetterGuestBook
+﻿using GuestBookLibrary.Models;
+
+/*
+Capture the info about the guests (assumption is at least one guest and unknown maximum of guests)
+Info to capture: First name, last name, message to the host
+Once done, loop through each guests and print their info
+*/
+
+namespace BetterGuestBook
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            List <GuestModel> guests = new List <GuestModel> ();//I create a List instead of array because I don't know how many "guests" will attend (array is when i know the number of "guests")
+            
+            string moreGuestsComing = string.Empty;
+
+            do // chose a do while loop, cuz I knew at least one guest is coming and this runs at least once
+            {
+                GuestModel guest = new GuestModel ();// this represents ONE singular guests
+
+                Console.WriteLine("What is your first name?");
+                guest.FirstName = Console.ReadLine();// i should check if name is "valid" but i presume its a paper guest name
+
+                Console.WriteLine("What is your last name?");
+                guest.LastName = Console.ReadLine();// i should check if name is "valid" but i presume its a paper guest name
+
+                Console.WriteLine("What message would you like to tell your host?");
+                guest.MessageToHost = Console.ReadLine();
+
+                Console.WriteLine("Are more guests coming? Yes/no");
+                moreGuestsComing = Console.ReadLine();
+
+                guests.Add (guest);// adding guest to the guests lists, else why it disappears
+
+                Console.Clear();
+
+
+            } while (moreGuestsComing.ToLower() == "yes");//runs as long as the answer is yes
+
+            foreach (GuestModel guest in guests)// loop through the guest list
+            {
+                Console.WriteLine(guest.GuestInfo);// get the readonly string from the GuestModels (better practice as we don't re-write the information)
+            }
             
         }
     }
 }
+
